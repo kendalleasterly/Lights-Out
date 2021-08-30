ebeccd77ac2be35fba5b21671378e0fe694efd9e
Kendall Easterly <qfletcher89@gmail.com>
2021-08-30 07:08:44 -0500

    Initial Commit

---
+++ Summer Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Summer Project
+//
+//  Created by Kendall Easterly on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Summer Project/Summer_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summer_ProjectApp.swift
+//  Summer Project
+//
+//  Created by Kendall Easterly on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Summer_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
